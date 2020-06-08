44d35399ab56e11ff8c8d89301ea13e8cfcc5ebc
sam ma <sam_newyork@163.com>
2020-06-08 23:56:23 +0800

    pattern match with case class and Seq list in scala

--- scala-fundamental/src/main/scala/org/lang/scala/ScalaPatternMatch.scala
+++ scala-fundamental/src/main/scala/org/lang/scala/ScalaPatternMatch.scala
@@ -73,6 +73,48 @@ object scalaPatternMatch {
         }
       }
 
+      // case中的guard语句，在case语句中使用_代替变量名称
+      for (i <- Seq(1, 2, 3, 4)) {
+        i match {
+          case _ if i%2 ==0 => println(s"even $i")
+          case _ => println(s"odd: $i")
+        }
+      }
+
+      // case类的匹配：可以在case语句中根据Person类成员字段属性值进行匹配
+      val alice = Person("Alice", 25, Address("1 Scala Lane", "Chicago", "USA"))
+      val bob = Person("Bob", 29, Address("2 Java Ave", "Miami", "USA"))
+      val charlie = Person("Charlie", 32, Address("3 Python Ct", "Boston", "USA"))
+      for (person <- Seq(alice, bob, charlie)) {
+        person match {
+          case Person("Alice", 25, Address(_, "Chicago", _)) => println("Hi Alice!")
+          case Person("Bob", 29, Address("2 Java Ave", "Miami", "USA")) => println("hi bob!")
+          case Person(name, age, _) => println(s"Who are you, $age year-old person and $name?")
+        }
+      }
+      // 对于(String, Double)组成的元组序列，表示商店里商品和价格 想通过for同时将其序列号打印出来，可使用zipWithIndex方法
+      val itemCosts = Seq(("Pencil", 0.52), ("Paper", 1.35), ("Notebook", 2.43))
+      val itemCostsIndices = itemCosts.zipWithIndex
+      for (itemCostIndex <- itemCostsIndices) {
+        itemCostIndex match {
+          case ((item, cost), index) => println(s"$index: $item costs $cost each")
+        }
+      }
+
+      // 匹配Seq中的元素序列，从最后一个元素Nil逆向访问所有元素列表 并最终转换为字符串
+      for (seq <- Seq(nonEmptyList, nonEmptyVector, nonEmptyMap.toSeq)) {
+        println(reverseSeqToString(seq))
+      }
+
+      // 使用滑动窗口windows()、windows()处理元素序列, 在提取序列时返回非固定数量的元素
+      for (seq <- Seq(nonEmptyList, emptyList, nonEmptyMap.toSeq)) {
+        println(windows(seq))
+      }
+
+      for (seq <- Seq(nonEmptyList, emptyList, nonEmptyMap.toSeq)) {
+        println(windows2(seq))
+      }
+
     }
   }
 
@@ -115,6 +157,29 @@ object scalaPatternMatch {
     // 使用::替代了+:符号，将List的head和tail内容拼接起来
     case head :: tail => s"($head :: ${listToString(tail)})"
     case Nil => "(Nil)"
+  }
+
+  // Simplistic address type. Using all strings is questionable, too
+  case class Address(street: String, city: String, country: String)
+  case class Person(name: String, age: Int, address : Address)
+
+  def reverseSeqToString[T](l: Seq[T]): String = l match {
+    case prefix :+ end => reverseSeqToString(prefix) + s" :+ $end"
+    case Nil => "Nil"
+  }
+
+  def windows[T](seq: Seq[T]): String = seq match {
+    // 提取Seq中的前两个元素，用_*表示其它剩余的元素(可变参数列表)
+    case Seq(head1, head2, _*) => s"($head1, $head2), " + windows(seq.tail)
+    // 还需要匹配Seq中只有一个元素的序列，当最后只剩余Nil时 对其进行直接拼接
+    case Seq(head, _*) => s"($head, _), " + windows(seq.tail)
+    case Nil => "Nil"
+  }
+
+  def windows2[T](seq: Seq[T]): String = seq match {
+    case head1 +: head2 +: tail => s"($head1, $head2), " + windows2(seq.tail)
+    case head +: tail => s"($head, _), " + windows2(tail)
+    case Nil => "Nil"
   }
 
 }
